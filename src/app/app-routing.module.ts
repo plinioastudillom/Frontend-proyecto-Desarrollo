216108d224ed1b47ea57bb2c209f0b441c3b31e5
Hugo Hernandez <hernandez.hb473@gmail.com>
2022-09-18 13:42:11 -0600

    se agregan complementos del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'Registro', pathMatch: 'full'
+  },
+  {
+    path: 'Registro',
+    loadChildren: () => import('./registro-estudiantes/registro-estudiantes.module').then(m => m.RegistroEstudiantesModule)
+  },
+  {
+    path: 'Login',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
